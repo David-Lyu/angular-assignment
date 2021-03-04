bdc3e2b2747bc1d7970f6c7d9f8bcf76d1148a0d
David-Lyu <lyu.david@yahoo.com>
2021-03-04 14:02:37 -0800

    started project added bootstrap

---
+++ databinding-assignment/src/app/game-control/game-control.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-game-control',
+  templateUrl: './game-control.component.html',
+  styleUrls: ['./game-control.component.css']
+})
+export class GameControlComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  startGame() {
+    console.log("start")
+  }
+  stopGame() {
+    console.log("stop")
+  }
+}
