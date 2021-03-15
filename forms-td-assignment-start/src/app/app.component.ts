7b54a96102fbf16d1831b9dd2fd1f87160e0bf96
David-Lyu <lyu.david@yahoo.com>
2021-03-14 22:34:20 -0700

    finished assignment for forms pt 2

--- forms-td-assignment-start/src/app/app.component.ts
+++ forms-td-assignment-start/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  subscriptionType = ["Basic", "Advanced", "Pro"];
+  @ViewChild('f') formElement;
 
+  onSubmit() {
+    console.log(this.formElement.value)
+  }
 }
